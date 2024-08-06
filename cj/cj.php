d3837474ef0e5f4589962938491f09b9b45b21be
Jonathan Sanchez <sanceznarval00@outlook.com>
2024-08-06 15:32:56 -0600

    Creación de carpeta de cronjobs

---
+++ cj/cj.php
@@ -0,0 +1,10 @@
+<?php
+$para1      = 'j.sanchez@veco.mx';
+$para2  =   'd.cuellar@veco.mx';
+$titulo    = 'Cron de prueba';
+$mensaje   = utf8_decode('Envío cada 1 minuto');
+$cabecera = utf8_decode('From: Jonathán Sánchez');
+
+/*mail($para1, $titulo, $mensaje, $cabecera);
+mail($para2, $titulo, $mensaje, $cabecera)*/
+?>
