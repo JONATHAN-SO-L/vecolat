b46e0d30911dea8fcb1352ac627acc23d9a66565
JONATHAN-SO-L <sanceznarval00@outlook.com>
2025-06-24 15:51:12 -0600

    Se actualizan los reportes de Mantenimiento Preventivo y Correctivo eliminando el cuadro de firmas y reestructurando el muestreo de información relevante para consulta

--- lib/Planta_pdf.php
+++ lib/Planta_pdf.php
@@ -47,19 +47,26 @@ $pdf->SetXY(12,50.3);
 $pdf->Cell (190,10,'',1,0,'C');
 
 $pdf->SetDrawColor(0,0,0);
+$pdf->SetFont("Arial","b",9);
+$pdf->SetXY(132,23);
+$pdf->Cell (25,7,utf8_decode('Fecha'),0,0,'C');
+$pdf->SetXY(152,23);
+$pdf->Cell (25,7,utf8_decode('Hora'),0,0,'C');
+
 $pdf->SetXY(110,28);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (46,7,'Fecha de levantamiento',1,1,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->SetXY(156,28);
-$pdf->Cell (35,7,utf8_decode($reg['datetime_inicio_sgc']),1,1,'C', true);
-$pdf->SetDrawColor(0,0,0);
+$pdf->Cell (25,7,utf8_decode('Levantamiento'),1,1,'C');
 $pdf->SetXY(110,35);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (46,7,'Hora de levantamiento',1,1,'C');
+$pdf->Cell (25,7,utf8_decode('Solución'),1,1,'C');
+
 $pdf->SetFont("Arial","",9);
-$pdf->SetXY(156,35);
-$pdf->Cell (35,7,utf8_decode($reg['hora']),1,1,'C', true);
+$pdf->SetXY(135,28);
+$pdf->Cell (20,7,utf8_decode($reg['datetime_inicio_sgc']),1,1,'C', true); # Fecha Levantamiento
+$pdf->SetXY(135,35);
+$pdf->Cell (20,7,utf8_decode($reg['datetime_fin_sgc']),1,1,'C', true); # Fecha Solución
+$pdf->SetXY(155,28);
+$pdf->Cell (20,7,utf8_decode($reg['hora']),1,1,'C', true); # Hora Levantamiento
+$pdf->SetXY(155,35);
+$pdf->Cell (20,7,utf8_decode($reg['hora_solucion']),1,1,'C', true); # Hora Solución
 
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetXY(12,65);
@@ -69,83 +76,49 @@ $pdf->SetFont("Arial","",9);
 $pdf->Cell (31,7,utf8_decode($reg['estado_ticket']),1,1,'L');
 
 $pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(80,65);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (30,10,utf8_decode('Hora Solución'),0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (25,7,utf8_decode($reg['hora_solucion']),1,1,'L');
-
-$pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(140,65);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (30,10,utf8_decode('Fecha Solución'),0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (20,7,utf8_decode($reg['datetime_fin_sgc']),1,1,'L');
-
-$pdf->SetDrawColor(0,0,0);
 $pdf->SetXY(15,80);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Nombre',0,0,'C');
+$pdf->Cell (35,10,'Usuario',0,0,'C');
 $pdf->SetFont("Arial","",9);
 $pdf->Cell (100,7,utf8_decode($reg['nombre_usuario']),1,1,'L');
 
 $pdf->SetXY(15,90);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,utf8_decode('Correo Electrónico'),0,0,'C');
+$pdf->Cell (35,10,utf8_decode('Área'),0,0,'C');
 $pdf->SetFont("Arial","",9);
-$pdf->Cell (65,7,utf8_decode($reg['email_cliente']),1,1,'L');
-
-$pdf->SetXY(110,90);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (25,10,utf8_decode('Área'),0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (50,7,utf8_decode($reg['area']),1,1,'L');
+$pdf->Cell (65,7,utf8_decode($reg['area']),1,1,'L');
 
 $pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,100);
+$pdf->SetXY(15,99);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Equipo',0,0,'C');
+$pdf->Cell (35,7,utf8_decode('Identificación'),0,0,'C');
+$pdf->SetXY(15,103);
+$pdf->Cell (35,7,utf8_decode('del Equipo'),0,0,'C');
+$pdf->SetXY(50,100);
 $pdf->SetFont("Arial","",9);
 $pdf->Cell (100,7,utf8_decode($reg['equipo']),1,1,'L');
 
 $pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,110);
+$pdf->SetXY(15,111);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Tipo',0,0,'C');
+$pdf->Cell (35,10,'Tipo de Falla',0,0,'C');
 $pdf->SetFont("Arial","",9);
+$pdf->SetXY(50,112);
 $pdf->Cell (100,7,utf8_decode($reg['tipo']),1,1,'L');
 
-/*$pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,120);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Prioridad',0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (100,7,utf8_decode($reg['prioridad']),1,1,'L');*/
-
-function quitar_acentos($cadena){
-    $originales  = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðòóôõöøùúûýýþÿ“‘';
-    $modificadas = 'aaaaaaaceeeeiiiidoooooouuuuybsaaaaaaaceeeeiiiidoooooouuuyybyOn';
-    $cadena = utf8_decode($cadena);
-    $cadena = strtr($cadena, utf8_decode($originales), $modificadas);
-    return utf8_encode($cadena);
-}
-
 $asunto = utf8_decode($reg['asunto']);
-//$asunto_new = quitar_acentos($asunto);
 $asunto_up = strtoupper($asunto);
 
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetFillColor(255, 255, 255);
-$pdf->SetXY(15,120);
+$pdf->SetXY(15,122);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Asunto',0,0,'C');
+$pdf->Cell (35,10,'Falla reportada',0,0,'C');
 $pdf->SetFont("Arial","",9);
-//$pdf->Cell (135,25,'',1,1,'C');
 $pdf->SetXY(50,123);
 $pdf->Cell (120,7, utf8_decode($asunto_up),1,1,'L');
 
 $mensaje = utf8_decode($reg['mensaje']);
-//$mensaje_new = quitar_acentos($mensaje);
 $mensaje_low = strtolower($mensaje);
 $mensaje_parse = ucfirst($mensaje_low);
 $mensaje_wordwrap = wordwrap($mensaje_parse, 500, "\n", false);
@@ -153,10 +126,11 @@ $mensaje_substr = substr($mensaje_wordwrap, 0, 400);
 
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetFillColor(255, 255, 255);
-$pdf->SetXY(15,135);
+$pdf->SetXY(14,135);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,10,'Mensaje ',0,0,'C');
+$pdf->Cell (35,10,utf8_decode('Descripción de la falla'),0,0,'C');
 $pdf->SetFont("Arial","",9);
+$pdf->SetXY(50,135);
 $pdf->Cell (135,25,'',1,1,'C');
 $pdf->SetXY(53,136);
 $pdf->MultiCell (130,4,utf8_decode($mensaje_substr),0,1,'L');
@@ -193,33 +167,17 @@ $pdf->Cell (135,25,'',1,1,'C');
 $pdf->SetXY(53,203);
 $pdf->MultiCell (120,5,utf8_decode($solu_substr),0,1,'L');
 
-			/*....:::::Foote:::::.....*/
-			$pdf->SetFillColor(203, 208, 211);
-			$pdf->SetXY(12,230);
-			$pdf->Cell (190,5,'Cuadro de Firmas ',1,0,'C',true);
-			$pdf->SetXY(12,235);
-			$pdf->Cell (95,5,utf8_decode('Resolvió Ticket'),1,0,'C',true);
-			$pdf->SetXY(107,235);
-			$pdf->Cell (95,5,utf8_decode('Conformidad del usuario'),1,0,'C',true);
-			$pdf->SetXY(107,240);
-			$pdf->Cell (95,15,utf8_decode($reg['nombre_usuario']),0,0,'C');
+$pdf->SetXY(11,228);
+$pdf->SetFont("Arial","b",9);
+$pdf->Cell (40,10,utf8_decode('Ingeniero de Soporte'),0,0,'C');
+$pdf->SetFont("Arial","",9);
+$pdf->SetXY(50,229);
+$pdf->Cell (50,7,utf8_decode($reg['observaciones']),1,1,'L');
 
-			$pdf->SetXY(12,235);
-			$pdf->Cell (95,15, '' ,1,0,'C');
-			$pdf->SetXY(12,240);
-			$pdf->Cell (95,15,utf8_decode($reg['observaciones']),0,0,'C');
-			$pdf->SetXY(107,235);
-			$pdf->Cell (95,15,'',1,0,'C');			
-
-
-
-			$pdf->SetY(249);
-			// Arial italic 8
-			$pdf->SetFont('Arial','I',8);
-			// Número de página
-			$pdf->Cell(0,10,utf8_decode('Página '.$pdf->PageNo().'/{nb}'),0,0,'C');
-				$pdf->SetX(160);
-					$pdf->Cell (30,10, 'REV. 5;09/23 FV-05-7-003' ,0,0,'C');
+$pdf->SetY(249);
+$pdf->SetFont('Arial','I',8);
+$pdf->SetX(160);
+$pdf->Cell (30,10, 'Rev.6; 06/25 FV-05-7-003' ,0,0,'C');
 				
 $pdf->Ln();
-$pdf->output();
+$pdf->output('I',utf8_decode($reg['serie'].' - '.$reg['datetime_fin_sgc']).'.pdf');
--- lib/Planta_pdf_prev.php
+++ lib/Planta_pdf_prev.php
@@ -70,56 +70,40 @@ $pdf->Cell (40,7,utf8_decode($reg['estado_ticket']),1,1,'L');
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetXY(15,80);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,7,'Nombre del Usuario',0,0,'C');
+$pdf->Cell (35,7,'Usuario',0,0,'C');
 $pdf->SetFont("Arial","",9);
 $pdf->Cell (100,7,utf8_decode($reg['usuario']),1,1,'L');
 
-$pdf->SetXY(15,95);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,7,utf8_decode('Correo Electrónico'),0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (100,7,utf8_decode($reg['email_cliente']),1,1,'L');
-
 $pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,110);
+$pdf->SetXY(15,94);
 $pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,7,utf8_decode('Número de Serie'),0,0,'C');
-$pdf->SetXY(15,113);
+$pdf->Cell (35,7,utf8_decode('Identificación'),0,0,'C');
+$pdf->SetXY(15,98);
 $pdf->Cell (35,7,utf8_decode('del Equipo'),0,0,'C');
-$pdf->SetXY(50,111);
+$pdf->SetXY(50,95);
 $pdf->SetFont("Arial","",9);
-$pdf->Cell (100,7,utf8_decode($reg['equipo']),1,1,'L');
-
-/*$pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,110);
-$pdf->SetFont("Arial","b",9);
-$pdf->Cell (35,7,'Serie Calidad',0,0,'C');
-$pdf->SetFont("Arial","",9);
-$pdf->Cell (100,7,utf8_decode($serial_cal['num_serie']),1,1,'L');*/
+$pdf->Cell (30,7,utf8_decode($reg['equipo']),1,1,'L');
 
 $pdf->SetDrawColor(0,0,0);
-$pdf->SetXY(15,125);
+$pdf->SetXY(15,111);
 $pdf->SetFont("Arial","b",9);
 $pdf->Cell (35,7,utf8_decode('Fecha de Ejecución'),0,0,'C');
 $pdf->SetFont("Arial","",9);
-$pdf->Cell (100,7,utf8_decode($reg['date_hecho_sgc']),1,1,'L');
+$pdf->Cell (30,7,utf8_decode($reg['date_hecho_sgc']),1,1,'L');
 
 
 $asunto= utf8_decode($reg['asunto']);
 $mantenimiento= utf8_decode($reg['mantenimiento']);
 $observaciones= $reg['observaciones'];
 
-$mantto_wordwrap = wordwrap($mantenimiento, 60, "\n", false);
-$mantto_substr = substr($mantto_wordwrap, 0, 500);
-
 /************************************************************************************************************* 
 *							       	Encabezado de las casillas de verificación								 *
 *************************************************************************************************************/
-$pdf->SetXY(134,137.5);
+$pdf->SetXY(134,121.5);
 $pdf->SetFont("Arial","b",9);
 $pdf->Cell (35,10,utf8_decode('Aplica'),0,0,'C');
 
-$pdf->SetXY(154,137.5);
+$pdf->SetXY(154,121.5);
 $pdf->SetFont("Arial","b",9);
 $pdf->Cell (35,10,utf8_decode('N/A'),0,0,'C');
 
@@ -127,126 +111,340 @@ $pdf->Cell (35,10,utf8_decode('N/A'),0,0,'C');
 *														Casillas											 *
 *************************************************************************************************************/
 // Actividad 1
-$pdf->SetXY(150,146);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,146);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_1']) {
+    case 'Aplica':
+    $pdf->SetXY(150,129);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,129);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,129);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,129);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,129);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,129);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 2
-$pdf->SetXY(150,151);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,151);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_2']) {
+    case 'Aplica':
+    $pdf->SetXY(150,135);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,135);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,135);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,135);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,135);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,135);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 3
-$pdf->SetXY(150,157);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,157);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_3']) {
+    case 'Aplica':
+    $pdf->SetXY(150,140);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,140);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,140);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,140);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,140);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,140);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 4
-$pdf->SetXY(150,161);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,161);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_4']) {
+    case 'Aplica':
+    $pdf->SetXY(150,144);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,144);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,144);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,144);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,144);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,144);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 5
-$pdf->SetXY(150,165);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,165);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_5']) {
+    case 'Aplica':
+    $pdf->SetXY(150,148);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,148);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,148);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,148);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,148);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,148);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 6
-$pdf->SetXY(150,169);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,169);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_6']) {
+    case 'Aplica':
+    $pdf->SetXY(150,152);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,152);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,152);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,152);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,152);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,152);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 7
-$pdf->SetXY(150,173);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,173);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_7']) {
+    case 'Aplica':
+    $pdf->SetXY(150,156);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,156);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,156);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,156);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,156);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,156);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 8
-$pdf->SetXY(150,177);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,177);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_8']) {
+    case 'Aplica':
+    $pdf->SetXY(150,160);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,160);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,160);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,160);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,160);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,160);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 9
-$pdf->SetXY(150,181);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,181);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+switch ($reg['actividad_9']) {
+    case 'Aplica':
+    $pdf->SetXY(150,164);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,164);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
 
-// Actividad 9
-$pdf->SetXY(150,185);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,185);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    case 'N/A':
+    $pdf->SetXY(150,164);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,164);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,164);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,164);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
 
 // Actividad 10
-$pdf->SetXY(150,189);
+switch ($reg['actividad_10']) {
+    case 'Aplica':
+    $pdf->SetXY(150,168);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,168);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,168);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,168);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,168);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,168);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
+
+// Actividad 11
+switch ($reg['actividad_11']) {
+    case 'Aplica':
+    $pdf->SetXY(150,172);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,172);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,172);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,172);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,172);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,172);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
+
+// Actividad 12
+switch ($reg['actividad_12']) {
+    case 'Aplica':
+    $pdf->SetXY(150,176);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    $pdf->SetXY(170,176);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+
+    case 'N/A':
+    $pdf->SetXY(150,176);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,176);
+    $pdf->Cell (3,3,utf8_decode('X'),1,0,'C');
+    break;
+    
+    default:
+    $pdf->SetXY(150,176);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    $pdf->SetXY(170,176);
+    $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+    break;
+}
+
+// Actividad 13
+/*$pdf->SetXY(150,197);
 $pdf->Cell (3,3,utf8_decode(''),1,0,'C');
-$pdf->SetXY(170,189);
-$pdf->Cell (3,3,utf8_decode(''),1,0,'C');
+$pdf->SetXY(170,197);
+$pdf->Cell (3,3,utf8_decode(''),1,0,'C');*/
 
 /*************************************************************************************************************/
+$mantto_wordwrap = wordwrap($mantenimiento, 60, "\n", false);
+$mantto_substr = substr($mantto_wordwrap, 0, 555);
 
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetFillColor(255, 255, 255);
-$pdf->SetXY(15,140);
+$pdf->SetXY(15,125);
 $pdf->SetFont("Arial","b",9);
 $pdf->Cell (35,10,'Actividades a realizar',0,0,'C');
 $pdf->SetFont("Arial","",9);
-$pdf->Cell (135,55,'',1,0,'C');
-$pdf->SetXY(52,145);
+$pdf->Cell (135,62,'',1,0,'C');
+$pdf->SetXY(52,128);
 $pdf->MultiCell (97,4,utf8_decode($mantto_substr),0,0,'L');
 
 $obs_low = strtolower($observaciones);
 $obs_parse = ucfirst($obs_low);
-$obs_wordwrap = wordwrap($obs_parse, 85, "\n", false);
+$obs_wordwrap = wordwrap($obs_parse, 200, "\n", false);
 $obs_substr = substr($obs_wordwrap, 0, 400);
 
 $pdf->SetDrawColor(0,0,0);
 $pdf->SetFillColor(255, 255, 255);
-$pdf->SetXY(15,200);
+$pdf->SetXY(15,193);
 $pdf->SetFont("Arial","b",9);
-$pdf->MultiCell (30,10,'Observaciones',0,0,'C');
+$pdf->Cell (35,10,'Observaciones',0,0,'C');
 $pdf->SetFont("Arial","",9);
-$pdf->SetXY(50,200);
+$pdf->SetXY(50,193);
 $pdf->Cell (135,20,'',1,1,'C');
-$pdf->SetXY(52,202);
-$pdf->MultiCell (120,4,utf8_decode($reg['observaciones']),0,1,'L');
+$pdf->SetXY(52,194);
+$pdf->MultiCell (120,4,utf8_decode($reg['observaciones']),0,1,'');
 
-			/*....:::::Footer:::::.....*/
-			$pdf->SetFillColor(203, 208, 211);
-			$pdf->SetXY(12,230);
-			$pdf->Cell (190,5,'Cuadro de Firmas ',1,0,'C',true);
-			$pdf->SetXY(12,235);
-			$pdf->Cell (95,5,utf8_decode('Resolvió Ticket'),1,0,'C',true);
-			$pdf->SetXY(107,235);
-			$pdf->Cell (95,5,utf8_decode('Conformidad del usuario'),1,0,'C',true);
-			$pdf->SetXY(107,245);
-			$pdf->Cell (95,5,utf8_decode($reg['usuario']),0,0,'C');
+$pdf->SetXY(15,219);
+$pdf->SetFont("Arial","b",9);
+$pdf->Cell (35,10,'Ingeniero de Soporte',0,0,'C');
+$pdf->SetXY(50,220);
+$pdf->SetFont("Arial","",9);
+$pdf->Cell (50,7,utf8_decode($res['nombre']),1,1,'L');
 
-			$pdf->SetXY(12,235);
-			$pdf->Cell (95,15, '' ,1,0,'C');
-			$pdf->SetXY(12,240);
-			$pdf->Cell (95,15,utf8_decode($res['nombre']),0,0,'C');
-			$pdf->SetXY(107,235);
-			$pdf->Cell (95,15,'',1,0,'C');				
-
-
-
-			$pdf->SetY(249);
-			// Arial italic 8
-			$pdf->SetFont('Arial','I',8);
-			// Número de página
-			$pdf->Cell(0,10,utf8_decode('Página '.$pdf->PageNo().'/{nb}'),0,0,'C');
-				$pdf->SetX(160);
-					$pdf->Cell (30,10, 'REV. 0;10/23 FV-05-7-005' ,0,0,'C');
+$pdf->SetY(249);
+$pdf->SetFont('Arial','I',8);
+$pdf->SetX(160);
+$pdf->Cell (30,10, 'REV.1; 06/25 FV-05-7-005' ,0,0,'C');
 				
 $pdf->Ln();
-$pdf->output();
+$pdf->output('I',utf8_decode($reg['serie'].' - '.$reg['date_hecho_sgc']).'.pdf');
