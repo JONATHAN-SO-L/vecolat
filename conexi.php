873a5f25adab8d79277b7a2ade183f1a2915f04e
Jonathán Sánchez <sanceznarval00@outlook.com>
2026-01-22 09:15:34 -0600

    Update conexi.php

--- conexi.php
+++ conexi.php
@@ -8,9 +8,9 @@
  $mysqli = new mysqli('localhost', "veco_dvi", "Vec83Dv19iSa@", 'veco_sims_devecchi');
  $mysqli -> set_charset("utf8"); 
  
-	 $user="veco_dvi";
-	 $pw="Vec83Dv19iSa@";
-	 $host="localhost";
-	 $db="veco_sims_devecchi";
+	 $user="";
+	 $pw="";
+	 $host="";
+	 $db="";
 	
 ?>
